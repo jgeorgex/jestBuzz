f2ff1467572613a2e2ea25d257e72b2e122c1671
Julian George <jules104@gmail.com>
2020-06-04 10:43:29 +0100

    returns 2 if input is 2

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -3,3 +3,7 @@ const fizzBuzz = require('./fizzBuzz');
 test('returns 1 if input is 1', () => {
   expect(fizzBuzz(1)).toBe(1);
 });
+
+test('returns 2 if input is 2', () => {
+  expect(fizzBuzz(2)).toBe(2);
+});
